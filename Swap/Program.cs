c503a198cf84f7d7cc27afaa6df35ac8183de446
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-11 12:17:34 +1200

    Add project files.

---
+++ Swap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter a,b");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before swap a=" + a + " b =" + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("After swap a=" + a);
+            Console.WriteLine("b=" + b);
+
+
+
+        }
+    }
+}
